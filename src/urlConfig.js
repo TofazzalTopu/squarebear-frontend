cabf3d5d2605791f620f9a7436bc4957e6ce89c6
Md Tofazzal Hossain <37403566+TofazzalTopu@users.noreply.github.com>
2021-11-13 23:48:51 +0600

    update url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,7 @@
 //for aws
-//export const BACKEND_API_URL = "http://13.54.117.26:8080/api";
-//export const FRONTEND___URL = "http://13.54.117.26:3000";
-//export const BACKEND_WEBSOCKET_URL = "http://13.54.117.26:8080";
+export const BACKEND_API_URL = "http://13.54.117.26:8080/api";
+export const FRONTEND___URL = "http://13.54.117.26:3000";
+export const BACKEND_WEBSOCKET_URL = "http://13.54.117.26:8080";
 //for locally
- export const BACKEND_API_URL = "http://localhost:8080/api";
- export const BACKEND_WEBSOCKET_URL = "http://localhost:8080";
+// export const BACKEND_API_URL = "http://localhost:8080/api";
+//export const BACKEND_WEBSOCKET_URL = "http://localhost:8080";
