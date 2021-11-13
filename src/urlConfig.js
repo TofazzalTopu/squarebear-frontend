365501f6b0bbc77ba1efd308281a05f1e6fe0683
Md Tofazzal Hossain <37403566+TofazzalTopu@users.noreply.github.com>
2021-11-14 00:01:17 +0600

    update url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,6 @@
 //for aws
 export const BACKEND_API_URL = "http://13.54.117.26:8080/api";
-export const FRONTEND___URL = "http://13.54.117.26:3000";
+//export const FRONTEND___URL = "http://13.54.117.26:3000";
 export const BACKEND_WEBSOCKET_URL = "http://13.54.117.26:8080";
 //for locally
 // export const BACKEND_API_URL = "http://localhost:8080/api";
